c8ea2965467129b2a9f72f92216cffbaaeac570d
Gowtham <gowtham@example.com>
2026-03-30 15:36:17 +0530

    Hardcode DB fallback for Railway

--- src/lib/mysql.ts
+++ src/lib/mysql.ts
@@ -1,11 +1,11 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  port: Number(process.env.MYSQL_PORT) || 3306,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST || "gondola.proxy.rlwy.net",
+  port: Number(process.env.MYSQL_PORT) || 57160,
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "djQLqFaBWnMdoRMYVphnzcnHQjNrcesx",
+  database: process.env.MYSQL_DATABASE || "railway",
   waitForConnections: true,
   connectionLimit: 10,
 });
